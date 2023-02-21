dd749e7cc6695e56e37e925b2905bb7e559222d5
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-02-21 13:20:58 +0800

    Add material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import * as React from "react";
+import Button from "@mui/material/Button";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function MyApp() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="tw-py-3 tw-h-screen tw-w-screen tw-bg-primary-default">
+      <div className="tw-grid tw-justify-center">
+        <Button variant="contained">Hello World</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
-
-export default App
