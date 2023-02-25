4c54dc2d23ac61770a0879a3f40288cc4d3db845
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-02-25 13:43:11 +0800

    Import component

---
+++ src/component/index.js
@@ -0,0 +1,19 @@
+import Navbar from "./navbar";
+import Footer from "./footer";
+import Button from "./button";
+import Card from "./card";
+import Title from "./title";
+import Description from "./description";
+import DashboardLayout from "./dashboard-layout";
+import TableLayout from "./table-layout";
+
+export {
+  Navbar,
+  Footer,
+  Button,
+  Card,
+  Title,
+  Description,
+  DashboardLayout,
+  TableLayout,
+};
