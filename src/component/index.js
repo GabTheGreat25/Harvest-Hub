777a851685efb859a7ab3778b597875e8110dd06
renzocabarios <renzocabarios@gmail.com>
2023-02-28 12:29:19 +0800

    Barrel components

--- src/component/index.js
+++ src/component/index.js
@@ -1,19 +1,23 @@
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
+import Sidenav from "./Sidenav";
+import DashboardLayout from "./DashboardLayout";
+import ListItem from "./ListItem";
 import Footer from "./footer";
 import Button from "./button";
 import Card from "./card";
 import Title from "./title";
 import Description from "./description";
-import DashboardLayout from "./dashboard-layout";
 import TableLayout from "./table-layout";
 
 export {
   Navbar,
+  Sidenav,
+  DashboardLayout,
   Footer,
   Button,
   Card,
   Title,
   Description,
-  DashboardLayout,
   TableLayout,
+  ListItem,
 };
