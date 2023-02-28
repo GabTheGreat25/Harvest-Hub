e8606a1dcfd7b53ebc3f27c81bd2a9306f63a849
renzocabarios <renzocabarios@gmail.com>
2023-02-28 15:12:34 +0800

    Add Datatable

--- src/component/index.js
+++ src/component/index.js
@@ -2,5 +2,6 @@ import Navbar from "./Navbar";
 import Sidenav from "./Sidenav";
 import DashboardLayout from "./DashboardLayout";
 import ListItem from "./ListItem";
+import Datatable from "./Datatable";
 
-export { Navbar, Sidenav, DashboardLayout, ListItem };
+export { Datatable, Navbar, Sidenav, DashboardLayout, ListItem };
