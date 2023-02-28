50cc471714fb796bb0cb68832e3342c9d484b7d7
renzocabarios <renzocabarios@gmail.com>
2023-02-28 12:43:17 +0800

    Update components

--- src/component/index.js
+++ src/component/index.js
@@ -2,22 +2,5 @@ import Navbar from "./Navbar";
 import Sidenav from "./Sidenav";
 import DashboardLayout from "./DashboardLayout";
 import ListItem from "./ListItem";
-import Footer from "./footer";
-import Button from "./button";
-import Card from "./card";
-import Title from "./title";
-import Description from "./description";
-import TableLayout from "./table-layout";
 
-export {
-  Navbar,
-  Sidenav,
-  DashboardLayout,
-  Footer,
-  Button,
-  Card,
-  Title,
-  Description,
-  TableLayout,
-  ListItem,
-};
+export { Navbar, Sidenav, DashboardLayout, ListItem };
